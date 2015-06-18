16e02adc214d37fd3533930a1d3cc27545c2383e
Vogel612 <Vogel612@gmx.de>
2015-06-18 22:36:55 +0200

    Added minor javadoc to Translation

--- src/main/java/de/vogel612/helper/data/Translation.java
+++ src/main/java/de/vogel612/helper/data/Translation.java
@@ -1,15 +1,37 @@
 package de.vogel612.helper.data;
 
+/**
+ * <p>
+ * A simple data holder class to organize Translations. This class exposes an
+ * immutable {@link #key}, an immutable {@link #rootValue} and a
+ * {@link #translation} for simplicity it allows changing the translation, which
+ * may be removed later, depending on how it works out.
+ * </p>
+ * 
+ * @author vogel612<<a href="mailto:vogel612@gmx.de">vogel612@gmx.de</a>>
+ */
 public class Translation {
+	/**
+	 * The Key identifying the Translation in the resx file. Immutable for
+	 * rather obvious purposes
+	 */
 	private final String key;
+	/**
+	 * The RootValue of a Translation, meaning the "original language".
+	 * Immutable since we're not here to change the original UI
+	 */
 	private final String rootValue;
+	/**
+	 * The actual Translation "Value".
+	 */
 	private String translation;
-	
+
 	public Translation(final String key, final String rootValue) {
 		this(key, rootValue, "");
 	}
-	
-	public Translation(final String key, final String rootValue, final String currentTranslation) {
+
+	public Translation(final String key, final String rootValue,
+			final String currentTranslation) {
 		this.key = key;
 		this.rootValue = rootValue;
 		this.setTranslation(currentTranslation);
@@ -28,7 +50,6 @@ public class Translation {
 	}
 
 	public void setTranslation(String translation) {
-		// Notify people??
 		this.translation = translation;
 	}
 }
