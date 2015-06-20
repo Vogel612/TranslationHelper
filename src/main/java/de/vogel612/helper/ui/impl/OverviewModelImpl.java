69bccde31a309a1a621fe199525cfd4a61acc15e
Vogel612 <Vogel612@gmx.de>
2015-06-20 02:10:04 +0200

    Preliminary Model Implementation to extract translations

--- src/main/java/de/vogel612/helper/ui/impl/OverviewModelImpl.java
+++ src/main/java/de/vogel612/helper/ui/impl/OverviewModelImpl.java
@@ -5,9 +5,12 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 import org.jdom2.Document;
+import org.jdom2.Element;
 import org.jdom2.JDOMException;
+import org.jdom2.filter.ElementFilter;
 import org.jdom2.input.SAXBuilder;
 
 import de.vogel612.helper.data.Translation;
@@ -15,12 +18,12 @@ import de.vogel612.helper.ui.OverviewModel;
 import de.vogel612.helper.ui.OverviewPresenter;
 
 public class OverviewModelImpl implements OverviewModel {
-	
+
 	private OverviewPresenter presenter;
 	private Document translationDocument;
-	private final ExecutorService fileOperationService = Executors.newSingleThreadExecutor();
+	private final ExecutorService fileOperationService = Executors
+			.newSingleThreadExecutor();
 
-	
 	@Override
 	public void register(OverviewPresenter p) {
 		presenter = p;
@@ -31,7 +34,8 @@ public class OverviewModelImpl implements OverviewModel {
 		Runnable buildDocument = () -> {
 			SAXBuilder documentBuilder = new SAXBuilder();
 			try {
-				this.translationDocument = documentBuilder.build(resxFile.toFile());
+				this.translationDocument = documentBuilder.build(resxFile
+						.toFile());
 				presenter.onParseCompletion();
 			} catch (JDOMException e) {
 				presenter.onException(e, "Unspecified Parsing error");
@@ -44,8 +48,14 @@ public class OverviewModelImpl implements OverviewModel {
 
 	@Override
 	public List<Translation> getTranslations() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Element> translationElements = translationDocument
+				.getContent(new ElementFilter("data"));
+		
+		return translationElements.stream().map(el -> {
+			final String key = el.getAttribute("name").getValue();
+			final String currentValue = el.getChildText("value");
+			return new Translation(key, currentValue, currentValue);
+		}).collect(Collectors.toList());
 	}
 
 }
