51f03f298558662ecce16e69b5e3df1b40d8f11b
Vogel612 <Vogel612@gmx.de>
2015-06-21 00:05:06 +0200

    Fixed minor bug with layout code

--- src/main/java/de/vogel612/helper/ui/impl/OverviewViewImpl.java
+++ src/main/java/de/vogel612/helper/ui/impl/OverviewViewImpl.java
@@ -25,13 +25,13 @@ public class OverviewViewImpl implements OverviewView {
 
 	private static final int DEFAULT_WIDTH = 1000;
 	private static final int DEFAULT_HEIGHT = 700;
-	
+
 	private final JFrame window;
 	private final JTable translationContainer;
 	private final JPanel menuBar;
-	
+
 	private OverviewPresenter presenter;
-	
+
 	public OverviewViewImpl() {
 		window = new JFrame("Rubberduck Translation Helper");
 		// FIXME: send that to the presenter so we can clean up and close shit!
@@ -39,7 +39,7 @@ public class OverviewViewImpl implements OverviewView {
 		translationContainer = new JTable();
 		menuBar = new JPanel();
 	}
-	
+
 	@Override
 	public void register(OverviewPresenter p) {
 		presenter = p;
@@ -51,10 +51,9 @@ public class OverviewViewImpl implements OverviewView {
 		window.setSize(new Dimension(DEFAULT_WIDTH, DEFAULT_HEIGHT));
 		window.setMinimumSize(new Dimension(800, 500));
 		window.setBackground(new Color(0.2f, 0.3f, 0.7f, 1.0f));
-		
+
 		addMenuBar();
 		addTranslationContainer();
-		window.pack();
 		window.doLayout();
 	}
 
@@ -66,7 +65,7 @@ public class OverviewViewImpl implements OverviewView {
 		constraints.fill = BOTH;
 		constraints.gridx = 0;
 		constraints.gridy = 1;
-		
+
 		JScrollPane scroller = new JScrollPane(translationContainer);
 		scroller.setMinimumSize(new Dimension(800, 400));
 		scroller.setSize(new Dimension(800, 400));
@@ -74,8 +73,10 @@ public class OverviewViewImpl implements OverviewView {
 	}
 
 	private void addMenuBar() {
-		menuBar.setMinimumSize(new Dimension(800, 100));
-		menuBar.setSize(new Dimension(800, 100));
+		Dimension menuBarDimensions = new Dimension(800, 100);
+		menuBar.setMinimumSize(menuBarDimensions);
+		menuBar.setPreferredSize(menuBarDimensions);
+		
 		menuBar.setBackground(new Color(0.4f, 0.2f, 0.4f, 0.2f));
 		GridBagConstraints constraints = new GridBagConstraints();
 		constraints.insets = new Insets(15, 15, 15, 15);
@@ -84,7 +85,9 @@ public class OverviewViewImpl implements OverviewView {
 		constraints.weightx = 1.0;
 		constraints.weighty = 0.0;
 		constraints.fill = BOTH;
+		
 		window.add(menuBar, constraints);
+		menuBar.setSize(menuBarDimensions);
 	}
 
 	@Override
@@ -100,7 +103,8 @@ public class OverviewViewImpl implements OverviewView {
 
 	@Override
 	public void showError(String title, String errorMessage) {
-		JOptionPane.showMessageDialog(window, errorMessage, title, JOptionPane.ERROR_MESSAGE);
+		JOptionPane.showMessageDialog(window, errorMessage, title,
+				JOptionPane.ERROR_MESSAGE);
 	}
 
 }
