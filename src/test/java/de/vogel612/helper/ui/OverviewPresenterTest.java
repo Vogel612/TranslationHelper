568eb699178d127703880039f6643ba4b5ab376e
Clemens Lieb <vogel612@gmx.de>
2016-11-07 20:15:38 +0100

    drop onException from tests

--- src/test/java/de/vogel612/helper/ui/OverviewPresenterTest.java
+++ src/test/java/de/vogel612/helper/ui/OverviewPresenterTest.java
@@ -1,13 +1,12 @@
 package de.vogel612.helper.ui;
 
-import static org.mockito.Mockito.*;
-
 import de.vogel612.helper.ui.jfx.JFXDialog;
-import javafx.application.Platform;
 import javafx.stage.Stage;
 import org.junit.Before;
 import org.junit.Test;
 import org.testfx.framework.junit.ApplicationTest;
+
+import static org.mockito.Mockito.*;
 
 @SuppressWarnings("ThrowableResultOfMethodCallIgnored")
 public class OverviewPresenterTest extends ApplicationTest {
@@ -21,7 +20,7 @@ public class OverviewPresenterTest extends ApplicationTest {
     @Before
     public void beforeTest() {
         overviewView = mock(OverviewView.class);
-        dialog = mock(JFXDialog.class);
+        dialog = spy(JFXDialog.DIALOG);
         projectView = mock(ProjectView.class);
 
         cut = new OverviewPresenter(overviewView, projectView);
@@ -32,21 +31,6 @@ public class OverviewPresenterTest extends ApplicationTest {
     public void show_callsShow_onView() {
         cut.show();
         verify(overviewView).show();
-        verifyNoMoreInteractions(overviewView, dialog);
-    }
-
-    @Test
-    public void onException_delegatesToView() {
-        final Exception e = mock(Exception.class);
-        final String message = "testingmessage";
-        final String errorMessage = "alsdkj";
-        doReturn(errorMessage).when(e).getMessage();
-
-        cut.onException(e, message);
-
-        Platform.runLater(() -> verify(dialog).info(message, errorMessage));
-        verify(e).getMessage();
-        sleep(60); // await verification
         verifyNoMoreInteractions(overviewView, dialog);
     }
 
